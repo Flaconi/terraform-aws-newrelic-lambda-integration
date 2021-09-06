602b46d3c7ce6d5ca987f77fce69a1f4219c023e
Sergey Novikov <sergey.novikov@flaconi.de>
2021-09-06 13:07:32 +0200

    Split data from main

--- main.tf
+++ main.tf
@@ -7,16 +7,6 @@ resource "random_string" "this" {
   special = false
 }
 
-data "aws_ssm_parameter" "newrelic_license_key" {
-  name            = var.newrelic_license_key_path
-  with_decryption = true
-}
-
-data "aws_ssm_parameter" "newrelic_account_number" {
-  name            = var.newrelic_account_number
-  with_decryption = true
-}
-
 resource "aws_s3_bucket" "lambda_newrelic_resource" {
   bucket_prefix = "lambda-newrelic-resource"
   acl           = "private"
