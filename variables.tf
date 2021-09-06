b974415106c91f1f551d2aa7747a71cbd019fb1f
Sergey Novikov <sergey.novikov@flaconi.de>
2021-09-06 21:01:56 +0200

    Update to TF0.13

--- variables.tf
+++ variables.tf
@@ -1,11 +1,14 @@
 variable "newrelic_license_key_path" {
+  type    = string
   default = ""
 }
 
 variable "newrelic_account_number" {
+  type    = string
   default = ""
 }
 
 variable "region" {
+  type    = string
   default = "eu-central-1"
 }
