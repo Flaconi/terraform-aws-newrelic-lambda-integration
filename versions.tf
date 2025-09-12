21ee953e03792ba020b0719e69366b90d7170728
Sergey Novikov <sergey.novikov@flaconi.de>
2025-09-12 15:35:42 +0200

    OPS-0 Update to aws v6

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.78"
+      version = "~> 6.12"
     }
     random = {
       source  = "hashicorp/random"
